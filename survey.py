cf3a6617f4ededce24e73efcdf8e6900455d35a2
dsbowen <dsbowen@wharton.upenn.edu>
2020-03-13 19:05:50 -0400

    first commit

--- survey.py
+++ survey.py
@@ -1,34 +1,14 @@
-"""Hemlock survey template"""
+"""Hemlock-CRT demonstration"""
+
+from hemlock_crt import CRT
+from hemlock_crt.items import ball_bat, machines, lily_pads
 
 from hemlock import *
-
-from random import shuffle
 
 @route('/survey')
 def Start(origin=None):
     b = Branch()
-    
-    p = Page(b)
-    Label(p, label='<p>This is a consent form.</p>')
-    Input(p, label='<p>Enter your MTurk ID.</p>', var='MTurkID', all_rows=True)
-
-    p = Page(b)
-    c = Check(
-        p,
-        label='<p>Which type of social media do you like most?</p>',
-        choices=['Facebook','Twitter','Instagram'],
-        var='SocialMedia'
-    )
-    shuffle(c.choices)
-    Validate.require(c)
-
-    p = Page(b)
-    i = Input(p, label='<p>Enter your name.</p>', var='Name', all_rows=True)
-    Validate.require(i)
-    i = Input(p, label='<p>Enter your age.</p>', var='Age', all_rows=True)
-    Validate.is_type(i, int)
-    Validate.min_val(i, 0)
-
+    CRT(b, ball_bat, machines, lily_pads)
     p = Page(b, terminal=True)
-    Label(p, label='<p>Your responses have been recorded. Your payment code is xyz.</p>')
+    Label(p, label='<p>End.</p>')
     return b
